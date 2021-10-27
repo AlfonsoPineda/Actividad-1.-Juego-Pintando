7d61a41d06127422f43462b4a63c775b240764ef
Gael Perez <gael@MacBook-Pro-de-Gael.local>
2021-10-26 22:50:40 -0500

    Added rectangle function

--- paint.py
+++ paint.py
@@ -34,14 +34,28 @@ def circle(start, end):
     begin_fill()
 
     import turtle
+
     turtle.circle(end.x - start.x)
 
     end_fill()
 
 
 def rectangle(start, end):
-    "Draw rectangle from start to end."
-    pass  # TODO
+    "Draw square from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(4):
+        if count % 2 == 0:
+            forward((end.x - start.x) * 2)
+            left(90)
+        else:
+            forward(end.x - start.x)
+            left(90)
+
+    end_fill()
 
 
 def triangle(start, end):
@@ -51,15 +65,15 @@ def triangle(start, end):
 
 def tap(x, y):
     "Store starting point or draw shape."
-    start = state['start']
+    start = state["start"]
 
     if start is None:
-        state['start'] = vector(x, y)
+        state["start"] = vector(x, y)
     else:
-        shape = state['shape']
+        shape = state["shape"]
         end = vector(x, y)
         shape(start, end)
-        state['start'] = None
+        state["start"] = None
 
 
 def store(key, value):
@@ -67,19 +81,19 @@ def store(key, value):
     state[key] = value
 
 
-state = {'start': None, 'shape': line}
+state = {"start": None, "shape": line}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
 listen()
-onkey(undo, 'u')
-onkey(lambda: color('black'), 'K')
-onkey(lambda: color('white'), 'W')
-onkey(lambda: color('green'), 'G')
-onkey(lambda: color('blue'), 'B')
-onkey(lambda: color('red'), 'R')
-onkey(lambda: store('shape', line), 'l')
-onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
-onkey(lambda: store('shape', rectangle), 'r')
-onkey(lambda: store('shape', triangle), 't')
+onkey(undo, "u")
+onkey(lambda: color("black"), "K")
+onkey(lambda: color("white"), "W")
+onkey(lambda: color("green"), "G")
+onkey(lambda: color("blue"), "B")
+onkey(lambda: color("red"), "R")
+onkey(lambda: store("shape", line), "l")
+onkey(lambda: store("shape", square), "s")
+onkey(lambda: store("shape", circle), "c")
+onkey(lambda: store("shape", rectangle), "r")
+onkey(lambda: store("shape", triangle), "t")
 done()
